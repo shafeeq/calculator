4d2d5ed31205b1e16b97bd8ccd3ee0c83ba40074
Bharadhwaj CN <bharadhwaj10@gmail.com>
2014-02-15 15:49:21 +0530

    Included calculator.h library and made minor documentations.

--- log.c
+++ log.c
@@ -1,3 +1,8 @@
+/*
+This function is indented to do the mathematical function Logarithm of a number and return its value.
+*/
+
+#include "calculator.h"
 #include<math.h>
 float log(float x) { // Function for implementing log
 	float l; // Variable l for storing the result
