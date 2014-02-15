264a71e1c69f848a1d6bb3e192bb5cd3b29ef27c
Bharadhwaj CN <bharadhwaj10@gmail.com>
2014-02-15 13:58:18 +0530

    Added Division

---
+++ div.c
@@ -0,0 +1,11 @@
+/*
+This function is indented to do the mathematical function Division of 2 numbers and return the quotient.
+*/
+
+#include "calculator.h"
+float division(float a, float b) { // Function for division
+	float q; // Variable 'q' for storing quotient
+	q = a / b; // Assinged value to 'q'
+	return q; // returning 'q'
+}
+
