1dec5caed73c38a7c5b7d079cdf7eaece35e86d4
ashwin713 <ashwinjkm94@gmail.com>
2014-02-15 14:22:02 +0530

    Modulus function has been added

---
+++ modulus.c
@@ -0,0 +1,8 @@
+#inlude "calculator"
+
+// This will accept two integers and return the modulus of the first number with respect to the second number \\
+
+int modulus(int x,int y)
+{
+    return x%y);
+}
