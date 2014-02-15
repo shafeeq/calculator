6c1dc7afd15660bb9b61c9864c304d108b40cb46
Shafeeq K <shafeeq94@gmail.com>
2014-02-15 15:37:03 +0530

    Add partial calculator.c

---
+++ calculator.c
@@ -0,0 +1,18 @@
+#include "calculator.h"
+#include <stdio.h>
+
+int main()
+{
+	int opt;
+	int a,b;
+	float x,y;
+	do{
+		printf("Welcome to Collaboraed Calculator!\n");
+		printf("Operations:\n");
+		printf("1.Add 2.Subtact 3.Multiply 4.Divide\n");
+		printf("5.pow(x,y) 6.log(x) 7.mod(x,y) 8.Exit\n");
+		printf("Enter choice: ");
+		scanf("%d", &opt);
+
+ 
+	
